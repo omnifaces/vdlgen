a696a450a1065bed720f13f3d8b7e7983f91eea0
Bauke Scholtz <balusc@gmail.com>
2026-03-20 09:53:57 -0400

    generate package-info

---
+++ src/main/java/org/omnifaces/vdl/package-info.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright OmniFaces
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+/**
+ * <p>
+ * Public API for Vdlgen, a compile-time annotation processor that generates Facelet Tag Library Descriptor
+ * ({@code .taglib.xml}) files from annotated Jakarta Faces component, converter, validator, behavior, tag handler, and
+ * function classes.
+ * <p>
+ * Start by placing {@link org.omnifaces.vdl.FaceletTagLibrary} on any class that participates in the tag library, then
+ * use the other annotations in this package to declare attributes, component configuration, and functions.
+ *
+ * @see org.omnifaces.vdl.FaceletTagLibrary
+ */
+package org.omnifaces.vdl;
